0ebfaef34268ce5bc427fabe70cc385c74955ea2
Pedro Lopes <hello@pedrolopes.net>
2014-03-26 23:05:54 +0000

    Initial Commit

---
+++ Selfix/LOPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOPAppDelegate.h
+//  Selfix
+//
+//  Created by Pedro Lopes on 26/03/14.
+//  Copyright (c) 2014 Pedro Lopes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Selfix/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Selfix
+//
+//  Created by Pedro Lopes on 26/03/14.
+//  Copyright (c) 2014 Pedro Lopes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LOPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LOPAppDelegate class]));
+    }
+}
